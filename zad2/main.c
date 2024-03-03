97281e43d0f77636216e13d4f4d265f0552b21db
unknown <tomaszhamerla@interia.pl>
2024-03-03 21:18:49 +0100

    fixed zad 2

--- zad2/main.c
+++ zad2/main.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 
-#define STUDENTS 5
 #define SUBJECTS 2
+#define STUDENTS 5
 
 int main() {
     int grades[SUBJECTS][STUDENTS];
-    float averages[SUBJECTS] = {0};
-
-
-    printf("Podaj oceny uczniow:\n");
+    float averages[SUBJECTS] = {0, 0};
+    printf("Please provide the students' grades\n");
     for (int i = 0; i < STUDENTS; i++) {
-        printf("Uczen %d:\n", i+1);
+        printf("Student: %d\n", i + 1);
         for (int j = 0; j < SUBJECTS; j++) {
-            printf("Ocena z przedmiotu %s: ", j == 0 ? "matematyki" : "fizyki");
+            printf("The grade of the subject of %s", j == 0 ? "mathematics" : "physics");
             scanf("%d", &grades[j][i]);
         }
     }
-
-
     for (int i = 0; i < SUBJECTS; i++) {
-        for (int j = 0; j < STUDENTS; j++) {
+        for (int j = 0; j < STUDENTS; ++j) {
             averages[i] += grades[i][j];
         }
         averages[i] /= STUDENTS;
     }
-
-
-    printf("\nSrednie ocen:\n");
-    printf("Matematyka: %.2f\n", averages[0]);
-    printf("Fizyka: %.2f\n", averages[1]);
-
+    printf("The average grade:\n");
+    printf("Mathematics: %.f\n", averages[0]);
+    printf("Physics: %.f\n", averages[1]);
     return 0;
 }
